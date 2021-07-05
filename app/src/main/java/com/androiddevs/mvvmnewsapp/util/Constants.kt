a9fd6906c8fb2279b090c7295f91cafef6a91332
Nebi <mam10122011@yahoo.com>
2021-07-05 18:05:38 +0300

    just finieshed

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "e6e293caafab4a129465fe8dfaba5232"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
